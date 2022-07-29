3fff9d74c21912fce338b67455752071b9872a08
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2022-07-29 19:55:57 +0600

    Add files via upload

---
+++ Domain to IP Adress.py
@@ -0,0 +1,12 @@
+# Domain-to-IP-addresses
+
+import socket
+import pyfiglet
+
+banner = pyfiglet.figlet_format("Domain    to    IP")
+print(banner)
+
+domain = input("Enter domain : ")
+ip = socket.gethostbyname(domain)
+
+print("IP : " + ip)
